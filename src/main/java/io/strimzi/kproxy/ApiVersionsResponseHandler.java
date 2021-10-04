b223b5bf0368e780b71a8b038749993ea6353e01
Tom Bentley <tbentley@redhat.com>
2021-10-04 12:37:51 +0100

    fipx

--- src/main/java/io/strimzi/kproxy/ApiVersionsResponseHandler.java
+++ src/main/java/io/strimzi/kproxy/ApiVersionsResponseHandler.java
@@ -25,34 +25,49 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 class ApiVersionsResponseHandler extends ChannelInboundHandlerAdapter {
-    private static final Logger LOGGER = LogManager.getLogger(KafkaProxyFrontendHandler.class);
+    private static final Logger LOGGER = LogManager.getLogger(ApiVersionsResponseHandler.class);
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         KafkaFrame frame = (KafkaFrame) msg;
         if (frame.apiKey() == ApiKeys.API_VERSIONS) {
             var resp = (ApiVersionsResponseData) frame.body();
-            for (var key : resp.apiKeys()) {
-                short apiId = key.apiKey();
-                if (ApiKeys.hasId(apiId)) {
-                    ApiKeys apiKey = ApiKeys.forId(apiId);
-                    short min = (short) Math.max(
-                            key.minVersion(),
-                            apiKey.messageType.lowestSupportedVersion());
-                    if (min != key.minVersion()) {
-                        LOGGER.trace("[{}] Use {} min version {} (was: {})", ctx.channel(), apiKey, min, key.maxVersion());
-                        key.setMinVersion(min);
-                    }
-
-                    short max = (short) Math.min(
-                            key.maxVersion(),
-                            apiKey.messageType.highestSupportedVersion());
-                    if (max != key.maxVersion()) {
-                        LOGGER.trace("[{}] Use {} max version {} (was: {})", ctx.channel(), apiKey, max, key.maxVersion());
-                        key.setMaxVersion(max);
-                    }
-                }
-            }
+            intersectApiVersions(ctx, resp);
         }
         super.channelRead(ctx, msg);
+    }
+
+    private static void intersectApiVersions(ChannelHandlerContext ctx, ApiVersionsResponseData resp) {
+        for (var key : resp.apiKeys()) {
+            short apiId = key.apiKey();
+            if (ApiKeys.hasId(apiId)) {
+                ApiKeys apiKey = ApiKeys.forId(apiId);
+                intersectApiVersion(ctx, key, apiKey);
+            }
+        }
+    }
+
+    /**
+     * Update the given {@code key}'s max and min versions so that the client uses APIs versions mutually
+     * understood by both the proxy and the broker.
+     * @param ctx The context.
+     * @param key The key data from an upstream API_VERSIONS response.
+     * @param apiKey The proxy's API key for this API.
+     */
+    private static void intersectApiVersion(ChannelHandlerContext ctx, ApiVersionsResponseData.ApiVersion key, ApiKeys apiKey) {
+        short mutualMin = (short) Math.max(
+                key.minVersion(),
+                apiKey.messageType.lowestSupportedVersion());
+        if (mutualMin != key.minVersion()) {
+            LOGGER.trace("[{}] Use {} min version {} (was: {})", ctx.channel(), apiKey, mutualMin, key.maxVersion());
+            key.setMinVersion(mutualMin);
+        }
+
+        short mutualMax = (short) Math.min(
+                key.maxVersion(),
+                apiKey.messageType.highestSupportedVersion());
+        if (mutualMax != key.maxVersion()) {
+            LOGGER.trace("[{}] Use {} max version {} (was: {})", ctx.channel(), apiKey, mutualMax, key.maxVersion());
+            key.setMaxVersion(mutualMax);
+        }
     }
 }
